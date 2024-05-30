dc733fabc0a9442e721aa01132bc7f8c9e8d1a41
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-05-30 23:20:15 +0530

    Add project files.

---
+++ BlogAppAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogAppAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAppAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
