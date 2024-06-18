a6b9dc55dc4ec28e148efaa1dd069b8dc8460873
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-06-18 09:27:04 +0530

    CHG | Update Category Action Method

--- BlogAppAPI/Controllers/CategoriesController.cs
+++ BlogAppAPI/Controllers/CategoriesController.cs
@@ -19,7 +19,7 @@ namespace BlogAppAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequestDto request)
         {
             // Map DTO to Domain Model
             var category = new Category
@@ -84,6 +84,36 @@ namespace BlogAppAPI.Controllers
             return Ok(response);
         }
 
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
+        {
+            // Convert DTO to Domain Model
+            var category = new Category
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await categoryRepository.UpdateAsync(category);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            //Convert Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+
+            return Ok(response);
+
+        }
 
     }
 }
--- BlogAppAPI/Repositiries/Interface/ICategoryRepository.cs
+++ BlogAppAPI/Repositiries/Interface/ICategoryRepository.cs
@@ -10,5 +10,7 @@ namespace BlogAppAPI.Repositiries.Interface
 
         Task<Category?> GetById(Guid id);
 
+        Task<Category?> UpdateAsync(Category category);
+
     }
 }
