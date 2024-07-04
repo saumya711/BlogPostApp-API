722b056503691bfc95625948b6b742190e45cbdc
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-07-04 21:20:52 +0530

    CHG | API- Add BlogPost Function(HTTP POST)

---
+++ BlogAppAPI/Repositiries/Interface/IBlogPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using BlogAppAPI.Models.Domain;
+
+namespace BlogAppAPI.Repositiries.Interface
+{
+    public interface IBlogPostRepository
+    {
+        Task<BlogPost> CreateAsync(BlogPost blogPost);
+    }
+}
